c30824217ae13dcbf3fcf349c1767f3de50f8f52
Ramiros73 <Ramiros73@gmail.com>
2023-01-14 10:07:40 +0400

    Initial Commit

---
+++ HW 2.04 ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.04 ColorizedApp
+//
+//  Created by Рамиль Хамидуллин on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
